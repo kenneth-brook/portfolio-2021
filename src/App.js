675d5f8335d3ba32a5d286ef03297d16a56021f3
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2021-01-18 22:40:28 -0500

    home page component started

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./sass/index.scss";
 
 import Header from "./components/Header";
 import Nav from "./components/Nav";
+import Home from "./components/Home";
 
 function App() {
 	return (
 		<div className='App'>
 			<Header />
 			<Nav />
+			<Home />
 		</div>
 	);
 }
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+	return (
+		<div className='homeWrap'>
+			<h1>Lotsa nifty info goes here</h1>
+		</div>
+	);
+}
+
+export default Home;
