f878d8c1e922c96a19d44877b096777e3bf508d3
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2021-01-18 21:59:23 -0500

    started header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./sass/index.scss";
 
+import Header from "./components/Header";
+
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<Header />
+		</div>
+	);
 }
 
 export default App;
