d1c46d758a431f58b37af908ca853f34e03a512f
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2021-01-19 22:22:05 -0500

    site cards rendering

---
+++ src/components/SiteCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function SiteCard(props) {
+	return (
+		<div className='card'>
+			<div className='titleHead'>
+				<h1>{props.title}</h1>
+			</div>
+			<div className='imageWrap'>
+				<img src={props.img} />
+			</div>
+		</div>
+	);
+}
+
+export default SiteCard;
--- src/components/Sites.js
+++ src/components/Sites.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import SiteCard from "./SiteCard";
+import { data } from "../data/data";
 
 function Sites() {
 	return (
 		<div className='sitesWrap'>
 			<h1>My Web Sites</h1>
+			{data.map((dat) => {
+				return <SiteCard key={dat.id} title={dat.title} img={dat.img} />;
+			})}
 		</div>
 	);
 }
