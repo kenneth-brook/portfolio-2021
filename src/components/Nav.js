9d7295d58a96f984e961ec9382c8a057a8e5df1f
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2021-01-19 20:45:35 -0500

    skills page component started

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 
 import Home from "./Home";
+import Skills from "./Skills";
 
 function Nav() {
 	return (
@@ -13,10 +14,21 @@ function Nav() {
 							Home
 						</NavLink>
 					</li>
+					<li>
+						<NavLink
+							className='link'
+							exact
+							to='/Skills'
+							activeClassName='active'
+						>
+							My Skills
+						</NavLink>
+					</li>
 				</ul>
 			</nav>
 			<div>
 				<Route exact path='/' component={Home} />
+				<Route exact path='/Skills' component={Skills} />
 			</div>
 		</div>
 	);
