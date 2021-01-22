9ad3a2bbcff0be0cf4ae7c96865fc67ba90b195f
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2021-01-21 23:25:07 -0500

    about me page created and linked

---
+++ src/components/Me.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Me() {
+	return (
+		<div className='meWrap'>
+			<h1>About Me</h1>
+		</div>
+	);
+}
+
+export default Me;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 import Home from "./Home";
 import Skills from "./Skills";
 import Sites from "./Sites";
+import Me from "./Me";
 
 function Nav() {
 	return (
@@ -35,12 +36,18 @@ function Nav() {
 							My Sites
 						</NavLink>
 					</li>
+					<li>
+						<NavLink className='link' exact to='/Me' activeClassName='active'>
+							About Me
+						</NavLink>
+					</li>
 				</ul>
 			</nav>
 			<div>
 				<Route exact path='/' component={Home} />
 				<Route exact path='/Skills' component={Skills} />
 				<Route exact path='/Sites' component={Sites} />
+				<Route exact path='/Me' component={Me} />
 			</div>
 		</div>
 	);
