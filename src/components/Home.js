c03c7a2bf3130b2576700081f366ea851b34b238
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2021-01-21 22:54:27 -0500

    info blocks on home page created

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,24 @@ import React from "react";
 function Home() {
 	return (
 		<div className='homeWrap'>
-			<h1>Lotsa nifty info goes here</h1>
+			<h1>Welcome to my home of code.</h1>
+			<div className='infoBlockWrap'>
+				<div className='infoBlock'>
+					<div className='blockHead'>
+						<h2>Block 1</h2>
+					</div>
+				</div>
+				<div className='infoBlock'>
+					<div className='blockHead'>
+						<h2>Block 2</h2>
+					</div>
+				</div>
+				<div className='infoBlock'>
+					<div className='blockHead'>
+						<h2>Block 3</h2>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
