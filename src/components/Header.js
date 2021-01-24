04ac87db44938acd4a0412c4f2ef8f43e5c8551c
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2021-01-23 23:17:12 -0500

    social icons added and linked

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaLinkedin, FaGithub, FaFacebook } from "react-icons/fa";
 
 function Header() {
 	return (
@@ -13,7 +14,29 @@ function Header() {
 				<div className='rightBlock'>
 					<h1>Where to find me, and my work.</h1>
 				</div>
-				<div className='icoBlock'></div>
+				<div className='icoBlock'>
+					<a
+						href='https://www.linkedin.com/in/kenneth-brook/'
+						target='_blank'
+						rel='noreferrer'
+					>
+						<FaLinkedin />
+					</a>
+					<a
+						href='https://github.com/kenneth-brook'
+						target='_blank'
+						rel='noreferrer'
+					>
+						<FaGithub />
+					</a>
+					<a
+						href='https://www.facebook.com/kennetic.concepts'
+						target='_blank'
+						rel='noreferrer'
+					>
+						<FaFacebook />
+					</a>
+				</div>
 			</div>
 		</header>
 	);
